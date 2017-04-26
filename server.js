a46b96bebd1d447207ec90cedc4b8bb12c3a1127
Mario Godinez Medina <futureweb2000@gmail.com>
2017-04-26 15:06:17 -0500

    Agregadas los verbos basicos para una api REST

--- server.js
+++ server.js
@@ -8,9 +8,27 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get('/hola/:name', function(req,res){
-	res.send({message: `Hello ${req.params.name}`})
+// app.get('/hola/:name', function(req,res){
+// 	res.send({message: `Hello ${req.params.name}`})
+// })
+
+app.get('/api/product', (req, res)=>{
+	res.status(200).send({ products: []})
 })
+
+app.delete('/api/product/:productName', (req, res)=>{
+
+})
+app.put('/api/product/:productName', (req, res)=>{
+
+})
+
+app.post('/api/product', (req, res)=>{
+	console.log(req.body)
+	res.status(200).send({message: 'Se ha agregado uno nuevo'})
+})
+
+
 
 
 app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`))
