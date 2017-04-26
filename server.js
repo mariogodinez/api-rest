9b6c7f44e37479ba2428d372289973382f399ff2
Mario Godinez Medina <futureweb2000@gmail.com>
2017-04-25 22:01:37 -0500

    Added bese project of Api rest

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+
+app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`))
