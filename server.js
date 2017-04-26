3a9d73a835567a93bbd8a13ea650187cf2a02870
Mario Godinez Medina <futureweb2000@gmail.com>
2017-04-25 22:07:38 -0500

    How to make a endpint with params

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+app.get('/hola/:name', function(req,res){
+	res.send({message: `Hello ${req.params.name}`})
+})
 
 
 app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`))
